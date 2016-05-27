ac050f5c7bbceb0ec3dad6a538b99e341a4758ee
kotcmm <kotcmm@gmail.com>
2016-05-27 20:47:34 +0800

    添加vs项目配置，添加iis支持

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ConsoleApplication
+namespace LearnDotnetCore
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
